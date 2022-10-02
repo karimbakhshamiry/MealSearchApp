7f035494a42de89e9d975efc8bc37afe797828a0
karimbakhshamiry <karimbakhshamiry.123@gmail.com>
2022-10-02 21:44:33 +0430

    changed api, style and coloring theme

--- asset/js/index.js
+++ asset/js/index.js
@@ -2,23 +2,46 @@ const ingredient = document.querySelector("#ingredient");
 const searchBtn = document.querySelector(".search");
 const resultDiv = document.querySelector(".recipes");
 
-searchBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+function searchForRecipes() {
   if (ingredient.value.length > 0) {
-    fetch(
-      `https://themealdb.com/api/json/v1/1/filter.php?i=${ingredient.value}`
-    )
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredient.value}&apiKey=4999d77d3638422984ad323e4348fc68`;
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        if (data.meals !== null) {
-          const recipeHtml = data.meals.map((element) => {
+        if (data.length > 0) {
+          const recipeHtml = data.map((element) => {
+            const missedIngredientsHtml = element.missedIngredients
+              .map((item) => {
+                return `<li>${item.original}</li>`;
+              })
+              .join("");
+
+            const usedIngredientsHtml = element.usedIngredients
+              .map((item) => {
+                return `<li>${item.original}</li>`;
+              })
+              .join("");
+
+            const ordersHtml = [
+              missedIngredientsHtml,
+              usedIngredientsHtml,
+            ].join("");
+            // console.log(ordersHtml);
             return `
               <div class="recipe">
-                <h3 class="food">${element.strMeal}</h3>
-                <img
-                  src="${element.strMealThumb}"
-                  alt="${element.strMeal} Picture"
-                />
+                <h4 class="food">${element.title}</h4>
+                <div class='image_container'>
+                  <img
+                  src="${element.image}"
+                  alt="${element.image} Picture"
+                  />
+                </div>
+                <div class='orders'>
+                  <h5>Ingredients</h5>
+                  <ul>
+                    ${ordersHtml}
+                  </ul>
+                </div>
               </div>
             `;
           });
@@ -30,5 +53,16 @@ searchBtn.addEventListener("click", (e) => {
 
         ingredient.value = "";
       });
+  }
+}
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchForRecipes();
+});
+
+ingredient.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    searchForRecipes();
   }
 });
